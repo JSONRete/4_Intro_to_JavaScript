586654801f390d5bd53ad92058f87c5169ed2a36
JSONRete <jsonrete@gmail.com>
2022-06-26 13:34:34 -0400

    entered 'Given Solution'

--- Lessons/Lesson_3_Conditionals/Quizes/quiz_3_3.js
+++ Lessons/Lesson_3_Conditionals/Quizes/quiz_3_3.js
@@ -26,3 +26,25 @@ if (musicians <= 0) {
 } else {
   console.log("this is a large group" );
 }
+
+// Given Solution:
+
+// Here is one of the possible solutions:
+
+// // change the value of `musicians` to test your conditional statements
+// var musicians = 0;
+
+// // your code goes here
+// if (musicians <= 0) {
+//     console.log("not a group");
+// } else if (musicians === 1) {
+//     console.log("solo");
+// } else if (musicians === 2) {
+//     console.log("duet");
+// } else if (musicians === 3) {
+//     console.log("trio");
+// } else if (musicians === 4) {
+//     console.log("quartet");
+// } else if (musicians >= 5) {
+//     console.log("this is a large group");
+// }
