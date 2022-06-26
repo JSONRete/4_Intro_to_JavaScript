a4e001cda1596e114581d17a903e2ead1c1f7b51
JSONRete <jsonrete@gmail.com>
2022-06-26 01:22:20 -0400

    added 'Given Solution'

--- Quizes/quiz_2_11.js
+++ Quizes/quiz_2_11.js
@@ -21,3 +21,11 @@ var adjective3 = "entertaining";
 var madLib = `The Intro to JavaScript course is ${adjective1}. James and Julia are so ${adjective2}. I cannot wait to work through the rest of this ${adjective3} content!`;
 
 console.log(madLib);
+
+// Given Solution:
+// Here is one of the possible solutions:
+// var adjective1 = "amazing";
+// var adjective2 = "fun";
+// var adjective3 = "entertaining";
+// var madLib = "The Intro to JavaScript course is "+ adjective1 +". James and Julia are so "+ adjective2 +". I cannot wait to work through the rest of this "+ adjective3 +" content!";
+// console.log(madLib);
