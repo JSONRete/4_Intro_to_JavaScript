7dc6e4014df6f1badd83af99bded122d3d93fb7a
JSONRete <jsonrete@gmail.com>
2022-07-01 06:54:53 -0600

    my code

--- Lessons/Lesson_3_Conditionals/Quizes/quiz_3_6.js
+++ Lessons/Lesson_3_Conditionals/Quizes/quiz_3_6.js
@@ -55,10 +55,14 @@
  * 
  * 6. Your code should not be empty
  */
- 
+
 // change the values of `flavor`, `vessel`, and `toppings` to test your code
 var flavor = "strawberry";
 var vessel = "cone";
 var toppings = "cookies";
 
 // Add your code here
+
+if((flavor === "chocolate" || flavor === "vanilla") && (vessel === "cone" || vessel === "bowl") && (toppings === "sprinkles" || toppings === "peanuts")) {
+    console.log(`I'd like two scoops of ${flavor} ice cream in a ${vessel} with ${toppings}.`)
+}
