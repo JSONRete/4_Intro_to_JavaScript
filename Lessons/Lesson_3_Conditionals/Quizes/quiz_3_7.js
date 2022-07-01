bd2629c05b24cb954a4604eadf5c37b9751518b2
JSONRete <jsonrete@gmail.com>
2022-07-01 16:52:43 -0600

    solid fully tested code

--- Lessons/Lesson_3_Conditionals/Quizes/quiz_3_7.js
+++ Lessons/Lesson_3_Conditionals/Quizes/quiz_3_7.js
@@ -23,9 +23,15 @@
 
 
 // change the values of `shirtWidth`, `shirtLength`, and `shirtSleeve` to test your code
-var shirtWidth = 23;
-var shirtLength = 30;
-var shirtSleeve = 8.71;
+// var shirtWidth = 23;
+// var shirtLength = 30;
+// var shirtSleeve = 8.71;
+
+var shirtWidth = 18;
+var shirtLength = 28;
+var shirtSleeve = 8.13;
+let size = "NA"
+
 
 /*
  * To gain confidence, check your code for the following combination of [shirtWidth, shirtLength, shirtSleeve, expectedSize]:
@@ -42,8 +48,29 @@ var shirtSleeve = 8.71;
 
 // WRITE YOUR CODE HERE
 
+if ((shirtWidth >= 18 && shirtWidth < 20) && (shirtLength >= 28 && shirtLength < 29) && (shirtSleeve >= 8.13 && shirtSleeve < 8.38)) {
+    size = ("S");
+}
+else if ((shirtWidth >= 20 && shirtWidth < 22) && (shirtLength >= 29 && shirtLength < 30) && (shirtSleeve >= 8.38 && shirtSleeve < 8.63)) {
+    size = ("M");
+}
+else if ((shirtWidth >= 22 && shirtWidth < 24) && (shirtLength >= 30 && shirtLength < 31) && (shirtSleeve >= 8.63 && shirtSleeve < 8.88)) {
+    size = ("L");
+}
+else if ((shirtWidth >= 24 && shirtWidth < 26) && (shirtLength >= 31 && shirtLength < 33) && (shirtSleeve >= 8.88 && shirtSleeve < 9.63)) {
+    size = ("XL");
+}
+else if ((shirtWidth >= 26 && shirtWidth < 28) && (shirtLength >= 33 && shirtLength < 34) && (shirtSleeve >= 9.63 && shirtSleeve < 10.13)) {
+    size = ("2XL");
+}
+else if ((shirtWidth >= 28) && (shirtLength >= 28) && (shirtSleeve >= 10.13)) {
+    size = ("3XL");
+}
+else {
+    size = "NA";
+}
 
-
+console.log(size);
 
 
 // https://video.udacity-data.com/topher/2017/January/586e969e_tshirt-guide/tshirt-guide.png
