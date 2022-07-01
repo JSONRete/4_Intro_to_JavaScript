def4798910baa019947a461bbf9abd804812a05a
JSONRete <jsonrete@gmail.com>
2022-07-01 10:49:16 -0600

    added 'Given Solution'

--- Lessons/Lesson_3_Conditionals/Quizes/quiz_3_6.js
+++ Lessons/Lesson_3_Conditionals/Quizes/quiz_3_6.js
@@ -66,3 +66,17 @@ let toppings = "peanuts";
 if((flavor === "chocolate" || flavor === "vanilla") && (vessel === "cone" || vessel === "bowl") && (toppings === "sprinkles" || toppings === "peanuts")) {
     console.log(`I'd like two scoops of ${flavor} ice cream in a ${vessel} with ${toppings}.`);
 }
+
+
+// Given Solution:
+// Here is one of the possible solutions:
+
+// // change the values of `flavor`, `vessel`, and `toppings` to test your code
+// var flavor = "strawberry";
+// var vessel = "cone";
+// var toppings = "cookies";
+
+// // Add your code here
+// if ((flavor === "vanilla" || flavor === "chocolate") && (vessel === "cone" || vessel === "bowl") && (toppings === "sprinkles" || toppings === "peanuts")){
+//         console.log ("I'd like two scoops of " + flavor + "ice cream in a " + vessel + "with " + toppings + ".")
+// }
