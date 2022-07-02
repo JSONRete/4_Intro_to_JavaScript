14d48fb9055fe656562c1b56931431ff910325d4
JSONRete <jsonrete@gmail.com>
2022-07-02 14:13:55 -0600

    added 'Given Solution'

--- Lessons/Lesson_4_Loops/Quizes/quiz_4_1.js
+++ Lessons/Lesson_4_Loops/Quizes/quiz_4_1.js
@@ -35,3 +35,23 @@ while (x <= 20) {
 
 
 
+// Given Solution:
+// Here is one of the possible solutions:
+
+// var x = 1;
+
+// // while loop with a stop condition
+// while (x <= 20) {
+//     if (x % 3 === 0 && x % 5 === 0) {
+//         console.log("JuliaJames");
+//     }else if (x % 5 === 0) {
+//         console.log("James");
+//     }else if (x % 3 === 0) {
+//         console.log("Julia");
+//     }
+//     else {
+//         console.log(x);
+//     }
+//     // increment x at the end of each loop
+//     x = x + 1 ; 
+// }
