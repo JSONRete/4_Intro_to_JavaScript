609fa10f7ea7ab05c6fede988005a5c3bfc9564b
JSONRete <jsonrete@gmail.com>
2022-07-01 21:55:50 -0600

    added 'Given Solution'

--- Lessons/Lesson_3_Conditionals/Quizes/quiz_3_9.js
+++ Lessons/Lesson_3_Conditionals/Quizes/quiz_3_9.js
@@ -57,3 +57,33 @@ console.log(`In 2015, a person with ${education} earned an average of $${salary.
 // a Master's degree earned an average of $69,732/year,
 // a Professional degree earned an average of $89,960/year,
 // and a Doctoral degree earned an average of $84,396/year.
+
+// Given Solution:
+// Here is one of the possible solutions:
+// // your code goes here
+// switch (education) {
+//     case "no high school diploma":
+//         salary = 25636;
+//         break;
+//     case "a high school diploma":
+//         salary = 35256;
+//         break;
+//     case "an Associate's degree":
+//         salary = 41496;
+//         break;
+//     case "a Bachelor's degree":
+//         salary = 59124;
+//         break;
+//     case "a Master's degree":
+//         salary = 69732;
+//         break;
+//     case "a Professional degree":
+//         salary = 89960;
+//         break;
+//     case "a Doctoral degree":
+//         salary = 84396;
+//         break;
+// }
+
+
+console.log("In 2015, a person with "+education+" earned an average of $"+salary.toLocaleString("en-US")+"/year.");
