707d88ddc10c22390c7de0165148d6d669b2450f
JSONRete <jsonrete@gmail.com>
2022-07-03 01:21:37 -0600

    added fixed working code

--- Lessons/Lesson_4_Loops/Quizes/quiz_4_6.js
+++ Lessons/Lesson_4_Loops/Quizes/quiz_4_6.js
@@ -13,6 +13,11 @@
  */
  
 // fix the for loop
-for (var k = 0 k < 200 k++) {
+// for (var k = 0 k < 200 k++) {
+//     console.log(k);
+// }
+for (var k = 0; k < 200; k++) {
     console.log(k);
 }
+
+
