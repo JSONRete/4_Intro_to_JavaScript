4096df8988c2bbf4692cc0d82ac1983d61d8a946
JSONRete <jsonrete@gmail.com>
2022-07-04 23:57:54 -0600

    added 'Given Solution' to quiz file

--- Lessons/Lesson_5_Functions/quiz_5_1.js
+++ Lessons/Lesson_5_Functions/quiz_5_1.js
@@ -22,3 +22,19 @@ function laugh() {
 
 console.log(laugh());
 
+// Given Solution:
+// Here is one of the possible solutions:
+
+// // function definition
+// function laugh() {
+//     // Take a variable with any name of your choice
+//     var sound = "hahahahahahahahahaha!"
+//     return sound;
+// }
+
+
+// // function calling
+// // Don't forget to put a pair of `( )` along with the function name, while calling
+// console.log(laugh());
+
+
