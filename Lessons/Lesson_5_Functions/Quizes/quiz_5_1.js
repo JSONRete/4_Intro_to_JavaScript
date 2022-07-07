6954eb5516fa3501d145d953a7b7d8db975649de
JSONRete <jsonrete@gmail.com>
2022-07-06 21:52:51 -0600

    created Quizes folder and moved Quiz files

---
+++ Lessons/Lesson_5_Functions/Quizes/quiz_5_1.js
@@ -0,0 +1,40 @@
+/*
+ * Programming Quiz: Laugh it Off 1 (5-1)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a `laugh()` function
+ * - Your `laugh()` function should return the correct output
+ * - Your code should print `\"hahahahahahahahahaha!\"` by calling the `laugh()` function inside `console.log()`
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+
+
+// your code goes here
+
+// console.log(/* call the laugh function */);
+
+function laugh() {
+    let sound = "hahahahahahahahahaha!";
+    return sound;
+}
+
+console.log(laugh());
+
+// Given Solution:
+// Here is one of the possible solutions:
+
+// // function definition
+// function laugh() {
+//     // Take a variable with any name of your choice
+//     var sound = "hahahahahahahahahaha!"
+//     return sound;
+// }
+
+
+// // function calling
+// // Don't forget to put a pair of `( )` along with the function name, while calling
+// console.log(laugh());
+
+
