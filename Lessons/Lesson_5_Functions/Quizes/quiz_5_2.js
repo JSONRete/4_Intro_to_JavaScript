2fecf0525175931e4a0b1e13bb07ef626df2248c
JSONRete <jsonrete@gmail.com>
2022-07-07 21:59:26 -0600

    my solution

--- Lessons/Lesson_5_Functions/Quizes/quiz_5_2.js
+++ Lessons/Lesson_5_Functions/Quizes/quiz_5_2.js
@@ -1,48 +1,49 @@
 /*
- * Programming Quiz: Laugh it Off 2 (5-2)
- *
- * Write a function called `laugh` with a parameter named `num` that represents the number of "ha"s to return.
- *
- * Note:
- *  - make sure your the final character is an exclamation mark ("!")
- *  - make sure that your function produces the correct results when it is called multiple times
+ * Programming Quiz: Build A Triangle (5-3)
  */
 
 /*
  * QUIZ REQUIREMENTS
- * - Your code should have a `laugh()` function
- * - Your `laugh()` function should have one parameter named `num`
- * - Your `laugh()` function should return the correct number of laughs
+ * - Your code should have a `buildTriangle()` function
+ * - Your `buildTriangle()` function should take one argument (or you can say parameter)
+ * - Your `laugh()` function should build the triangle as describe above
  */
-let sound = "";
-function laugh(num) {
-    n = 0;
-    while (n < num ) {
-        n++;
-        sound += "ha";
-    }
-        sound = `${sound}!`; 
-    return sound; 
-}
-console.log(laugh(3));
 
-// Given Solution:
 
-// Here is one of the possible solutions:
-// var sound = "" ; 
-// function laugh(num) {
-//     for (var x = 0 ; x < num ; x++) {
-//         sound = sound + "ha" ;  
+// creates a line of * for a given length
+// function makeLine(length) {
+//     var line = "";
+//     for (var j = 1; j <= length; j++) {
+//         line += "* ";
 //     }
-//     sound = sound +"!"; 
-//     return sound; 
+//     return line + "\n";
 // }
 
-// console.log(laugh(3)) ;
+// your code goes here.  Make sure you call makeLine() in your own code.
+
+
+// test your code by uncommenting the following line
+//console.log(buildTriangle(10));
+
+function makeLine(length) {
+  var line = "";
+  for (var j = 1; j <= length; j++) {
+    line += "* ";
+  }
+  return line + "\n";
+}
 
 
 
-
-
+function buildTriangle(length) {
+let triangle = "";
+let line = 0;
+    while (line < length ) {
+        line++;
+      triangle = triangle + makeLine(line);
+    }
+    return triangle; 
+}
+console.log(buildTriangle(10));
 
 
