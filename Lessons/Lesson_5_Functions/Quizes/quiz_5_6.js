41c3fd254c1240fee8545508c56729aea6a5f148
JSONRete <jsonrete@gmail.com>
2022-07-09 12:27:34 -0600

    added 'Given Solution' to quiz file

--- Lessons/Lesson_5_Functions/Quizes/quiz_5_6.js
+++ Lessons/Lesson_5_Functions/Quizes/quiz_5_6.js
@@ -32,3 +32,25 @@ emotions("happy", function(num) {
     sound = `${sound}!`; 
     return sound; 
 });
+
+
+// Given Solution:
+// Here is one of the possible solutions:
+
+// // emotions() function definition
+// function emotions(myString, myFunc) {
+//     console.log("I am " + myString + ", " + myFunc(2));
+// }
+
+// // Call the emotions() function with two arguments
+// // Argument 1 - "happy" string
+// // Argument 2 - an inline function expression
+// emotions ("happy", function(num) {
+//     var sound = ""; // Local variable
+//     //Iterate
+//     for (var i = 0 ; i < num ; i++) {
+//         sound = sound + "ha" ;  
+//     }
+//     sound = sound +"!"; 
+//     return sound; 
+// });
