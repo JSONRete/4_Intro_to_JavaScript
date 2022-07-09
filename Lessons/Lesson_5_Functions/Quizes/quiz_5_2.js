25fa4189a3b6b0a6c85670841bb4d2bae889c6e3
JSONRete <jsonrete@gmail.com>
2022-07-08 21:53:15 -0600

    fixed code

--- Lessons/Lesson_5_Functions/Quizes/quiz_5_2.js
+++ Lessons/Lesson_5_Functions/Quizes/quiz_5_2.js
@@ -24,52 +24,12 @@ function laugh(num) {
         sound = `${sound}!`; 
     return sound; 
 }
-console.log(laugh(3));
-
-// Given Solution:
-
-// Here is one of the possible solutions:
-// var sound = "" ; 
-// function laugh(num) {
-//     for (var x = 0 ; x < num ; x++) {
-//         sound = sound + "ha" ;  
-//     }
-//     sound = sound +"!"; 
-//     return sound; 
-// }
-
-// console.log(laugh(3)) ;
+ console.log(laugh(3));
+ 
 
 
-// Given Solution:
 
-// Here is one of the possible solutions:
-// // creates a line of * for a given length
-// function makeLine(length) {
-//     var line = "";
-//     for (var j = 1; j <= length; j++) {
-//         line += "* ";
-//     }
-//     return line + "\n";
-// }
 
-// // your code goes here.  Make sure you call makeLine() in your own code.
-// function buildTriangle(length) {
-//     // Let's build a huge string equivalent to the triangle
-//     var triangle = "";
 
-//     //Let's start from the topmost line
-//     var lineNumber = 1;
 
-//     for(lineNumber=1; lineNumber<=length; lineNumber++){
-//         // We will not print one line at a time.
-//         // Rather, we will make a huge string that will comprise the whole triangle
-//         triangle = triangle + makeLine(lineNumber);
-//     }
-//     return triangle;
-// }
 
-// // test your code by uncommenting the following line
-// // Note that the function buildTriangle() must return a string 
-// // because the console.log() accepts a string argument
-// console.log(buildTriangle(10));
