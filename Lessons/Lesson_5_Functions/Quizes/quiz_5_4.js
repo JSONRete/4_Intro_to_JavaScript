5a9468eda83d5730bcb7248707eb256675f1195d
JSONRete <jsonrete@gmail.com>
2022-07-09 09:55:34 -0600

    added 'Given Solution'

--- Lessons/Lesson_5_Functions/Quizes/quiz_5_4.js
+++ Lessons/Lesson_5_Functions/Quizes/quiz_5_4.js
@@ -27,3 +27,24 @@ let laugh = function(num) {
 };
 
 console.log(laugh(10));
+
+// Given Solution:
+
+// Here is one of the possible solutions:
+
+// // Declare an empty string
+// var sound = "" ; 
+
+// // An anonymous function expression stored in the variable `laugh`
+// var laugh = function(num) {
+
+//     //Iterate
+//     for (var x = 0 ; x < num ; x++) {
+//         sound = sound + "ha" ;  
+//     }
+//     sound = sound +"!"; 
+//     return sound; 
+// }
+
+// // It is essential that the function must return a string
+// console.log(laugh(3)) ;
