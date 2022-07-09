47e5d9fcdfbe74ea765ab8a9fcd447cef4062fd8
JSONRete <jsonrete@gmail.com>
2022-07-09 11:07:33 -0600

    added 'Given Solution'

--- Lessons/Lesson_5_Functions/Quizes/quiz_5_5.js
+++ Lessons/Lesson_5_Functions/Quizes/quiz_5_5.js
@@ -17,4 +17,17 @@
 let cry = function sound() {
     return "boohoo!"
 };
-console.log(cry()); 
+console.log(cry());
+
+// Given Solution:
+// Here is one of the possible solutions:
+// var cry = function myFunction(){
+//     var sound = "boohoo!";
+//     return sound;
+//     };
+
+// // Call the function using the variable name, not the function name
+// console.log(cry());
+
+// // This statement is also acceptable, but it won't print anything on your console
+// // cry();
