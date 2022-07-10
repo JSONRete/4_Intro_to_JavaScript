057c187498c717c1bd7c3d0889770ef5faa2f3af
JSONRete <jsonrete@gmail.com>
2022-07-10 16:58:03 -0400

    added 'Given Solution'

--- Lessons/Lesson_6_Arrays/Quizes/quiz_6_1.js
+++ Lessons/Lesson_6_Arrays/Quizes/quiz_6_1.js
@@ -15,3 +15,21 @@
 
 udaciFamily = ["Julia", "James", "JaSON"];
 console.log(udaciFamily);
+
+
+// Given Solution:
+// Here is one of the possible solutions:
+
+// // You can create an arrray in different ways
+// // Method 1
+// // var udaciFamily = new Array("Julia", "James", "Yourname"); 
+
+// // Method 2
+// var udaciFamily = ["Julia", "James", "Yourname"]; 
+
+// // Iterate over the array elements
+// // We can find the size of an array using its `length` property
+// // Remember that the indexing of elements in an array starts from 0
+// for (var index = 0; index<udaciFamily.length; index++){
+//     console.log(udaciFamily[index]);
+// }
