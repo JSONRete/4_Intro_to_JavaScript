c067d420bb8fb5296282370d9b3c3c3f82a4c151
JSONRete <jsonrete@gmail.com>
2022-07-10 18:25:05 -0400

    added 'Given Solution'

--- Lessons/Lesson_6_Arrays/Quizes/quiz_6_2.js
+++ Lessons/Lesson_6_Arrays/Quizes/quiz_6_2.js
@@ -9,7 +9,7 @@
  * - Your code should print `crew` to the console as an array. Do not iterate over the elements. 
  * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
  */
- 
+
 var captain = "Mal";
 var second = "Zoe";
 var pilot = "Wash";
@@ -19,5 +19,15 @@ var mechanic = "Kaylee";
 
 // your code goes here
 
- crew = [captain, second, pilot, companion, mercenary, mechanic]
- console.log(crew)
+crew = [captain, second, pilot, companion, mercenary, mechanic]
+console.log(crew)
+
+//  Given Solution:
+//  Here is one of the possible solutions:
+// var crew = new Array(captain, second, pilot, companion, mercenary, mechanic);
+
+// // Another method
+// // var crew = [captain, second, pilot, companion, mercenary, mechanic];
+
+// // Print
+// console.log(crew);
