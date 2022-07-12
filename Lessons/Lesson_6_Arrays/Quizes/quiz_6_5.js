ba7deda60ff722175c52ffd6829ad8720aa75645
JSONRete <jsonrete@gmail.com>
2022-07-12 13:03:14 -0400

    added 'Given Solution' to quiz

--- Lessons/Lesson_6_Arrays/Quizes/quiz_6_5.js
+++ Lessons/Lesson_6_Arrays/Quizes/quiz_6_5.js
@@ -21,3 +21,16 @@ function hasEnoughPlayers(team) {
 
 var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
 console.log(hasEnoughPlayers(team));
+
+
+// Given Solution:
+
+// var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+
+// function hasEnoughPlayers(arrayInstance) {
+//     if (arrayInstance.length >= 7)
+//         return true; // true is a boolean value. Do not return a string "true"
+//     else 
+//         return false; // false is also a boolean value. Do not return a string "false"
+// }
+// console.log(hasEnoughPlayers(team));
