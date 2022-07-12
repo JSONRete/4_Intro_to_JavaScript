f8f9c5a41879d4b1118f9849f2edba517d8bcb93
JSONRete <jsonrete@gmail.com>
2022-07-11 23:54:24 -0400

    add 'My Solution' & 'Given Solution'

--- Lessons/Lesson_6_Arrays/Quizes/quiz_6_3.js
+++ Lessons/Lesson_6_Arrays/Quizes/quiz_6_3.js
@@ -17,3 +17,23 @@ prices[0] = 6;
 prices[2] = 7;
 prices[6] = 5;
 console.log(prices);
+
+
+// Given Solution:
+
+// Here is one of the possible solutions:
+// for (var index = 0; index<prices.length; index++){
+//     //change the value of first element
+//     if(index===0){
+//         prices[index]=11;
+//     }
+//     //change the value of third element
+//     else if(index===2){
+//         prices[index]=33;
+//     }
+//     //change the value of seventh element
+//     else if(index===6){
+//         prices[index]=77;
+//     }
+// }
+// console.log(prices);
