2448a0bbb4a537a3eacf9296749abe8833db8165
JSONRete <jsonrete@gmail.com>
2022-07-12 14:29:58 -0400

    added 'Given Solution' to quiz

--- Lessons/Lesson_6_Arrays/Quizes/quiz_6_6.js
+++ Lessons/Lesson_6_Arrays/Quizes/quiz_6_6.js
@@ -28,3 +28,16 @@ var shepherd = "Book";
 crew.push(doctor, sister, shepherd);
 
 console.log(crew);
+
+// Given Solution:
+// Here is one of the possible solutions:
+
+// // your code goes here
+// crew.push(doctor); // "Simon" gets added at the end of the array
+// // We have intentionally not captured the return of the push() method
+
+// crew.push(sister); // "River" gets added at the end of the array
+// crew.push(shepherd); // "Book" gets added at the end of the array
+
+// console.log(crew);
+
