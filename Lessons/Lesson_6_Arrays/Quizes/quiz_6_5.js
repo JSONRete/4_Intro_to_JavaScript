b568447764c2c1f68bb80b13596601191afb10ff
JSONRete <jsonrete@gmail.com>
2022-07-12 13:01:06 -0400

    added  'My Solution' to quiz

--- Lessons/Lesson_6_Arrays/Quizes/quiz_6_5.js
+++ Lessons/Lesson_6_Arrays/Quizes/quiz_6_5.js
@@ -11,5 +11,13 @@
  
 // your code goes here
 
+function hasEnoughPlayers(team) {
+    if (team.length >= 7)
+        return true;
+    else
+        return false;
+}
+
+
 var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
 console.log(hasEnoughPlayers(team));
