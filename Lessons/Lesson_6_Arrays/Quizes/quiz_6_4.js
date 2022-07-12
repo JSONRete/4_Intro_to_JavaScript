82f28d87d00264633d0c8701679eabfbd2e5cbad
JSONRete <jsonrete@gmail.com>
2022-07-12 08:49:33 -0400

    adde 'My Solution'

--- Lessons/Lesson_6_Arrays/Quizes/quiz_6_4.js
+++ Lessons/Lesson_6_Arrays/Quizes/quiz_6_4.js
@@ -10,3 +10,8 @@
 var rainbow = ['Red', 'Orange', 'Blackberry', 'Blue'];
 
 // your code goes here
+
+rainbow.splice(2, 1, "Yellow", "Green");
+rainbow.splice(5, 0, "Purple");
+
+console.log(rainbow);
