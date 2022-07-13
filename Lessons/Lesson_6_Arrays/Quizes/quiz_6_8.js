b2f90b94d00a853175941103f9e544e41daf9b9a
JSONRete <jsonrete@gmail.com>
2022-07-13 16:45:00 -0400

    added 'My Solution' to quiz

--- Lessons/Lesson_6_Arrays/Quizes/quiz_6_8.js
+++ Lessons/Lesson_6_Arrays/Quizes/quiz_6_8.js
@@ -16,3 +16,10 @@ var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
 ];
 
 // Write your code here
+test.forEach(function(item, index) {
+  if(item % 3 === 0)
+  		// item = item + 100;
+			test[index]=test[index]+100;
+});
+
+console.log(test);
