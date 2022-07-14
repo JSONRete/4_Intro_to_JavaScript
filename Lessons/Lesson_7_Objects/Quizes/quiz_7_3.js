0e2df30241bad2af2341299018ee53496fabf08d
JSONRete <jsonrete@gmail.com>
2022-07-14 15:26:01 -0400

    added 'My Solution' to quiz code

--- Lessons/Lesson_7_Objects/Quizes/quiz_7_3.js
+++ Lessons/Lesson_7_Objects/Quizes/quiz_7_3.js
@@ -27,6 +27,8 @@ var savingsAccount = {
         }
     },
     // your code goes here
+    printAccountSummary: function() { return `Welcome!\nYour balance is currently $${savingsAccount.balance} and your interest rate is ${savingsAccount.interestRatePercent}%.`;
+    }
 };
 
 console.log(savingsAccount.printAccountSummary());
