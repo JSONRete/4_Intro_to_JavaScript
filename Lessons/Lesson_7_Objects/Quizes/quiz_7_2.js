43b7dbada409adaf1b21c1ed54fc22738c1371fb
JSONRete <jsonrete@gmail.com>
2022-07-14 13:48:05 -0400

    added starter code to empty quiz file

---
+++ Lessons/Lesson_7_Objects/Quizes/quiz_7_2.js
@@ -0,0 +1,8 @@
+/*
+ * Programming Quiz: Menu Items (7-2)
+ * Create an object named `breakfast`. 
+ * The object should contain properties for the `name`, `price`, and `ingredients`.
+ * Print the entire object on the console
+ */
+
+// your code goes here
